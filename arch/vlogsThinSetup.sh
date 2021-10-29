c952468b09d4d3ee14b5bfb051ffd00d9660546e
Victor Bruno Paro <victorbparo@gmail.com>
2021-10-29 20:23:19 -0300

    First commit. Only a test arch script currently

---
+++ arch/vlogsThinSetup.sh
@@ -0,0 +1,25 @@
+# Installing dependencies
+sudo pacman -S curl git zsh
+
+# Changing default shell to zsh
+chsh -s /bin/zsh && zsh
+
+# Installing oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+
+# Installing zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Creating aliases and other configurations
+echo alias cls="clear" >> $HOME/.zshrc
+
+finalMessage = "The script has finished it's execution.
+\nNow, there are just a few manual steps you need to perform:\n
+For the zsh autocomplete and syntax highlighting plugins, edit the $HOME/.zshrc file, and in the oh-my-zsh plugins section,\n
+add zsh-autosuggestions and zsh-syntax-highlighting."
+
+echo $finalMessage
+
+# Written by Victor Paro (Terrible-Developer). This script
+# currently has no license, but if it ends up having, it will have a free license like GPL or Apache.
